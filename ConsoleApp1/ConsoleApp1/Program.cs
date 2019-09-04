c2f1cb0a41a555bbbf5919406228a6f3d5b5209c
Andrey Samets <andrey_samets@mail.ru>
2019-09-04 11:25:25 +0300

    Age

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,12 +10,14 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string first, last;
+            string first, last, age;
             Console.WriteLine("Enter your first name: ");
             first = Console.ReadLine();
             Console.WriteLine("Enter your last name: ");
             last = Console.ReadLine();
-            Console.WriteLine("Hello, {0} {1}", first, last);
+            Console.WriteLine("Enter your age: ");
+            age = Console.ReadLine();
+            Console.WriteLine("Hello, {0} {1} {2}", first, last, age);
             Console.ReadLine();
         }
     }
